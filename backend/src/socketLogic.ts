f9a77e931e61dc895536dd0d29f34dc8e2fa535a
MRSevim <lionrasit@gmail.com>
2024-12-09 19:40:34 +0300

    added clg for debugging

--- backend/src/socketLogic.ts
+++ backend/src/socketLogic.ts
@@ -149,7 +149,7 @@ export const runSocketLogic = (io: any) => {
 
       // Check how many sockets are in the room
       const roomSockets = io.sockets.adapter.rooms.get(roomId);
-
+      console.log(roomSockets);
       if (!roomSockets) {
         // If no sockets are left, remove the game from memory
         removeGameFromMemory(roomId);
