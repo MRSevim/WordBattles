3dba6368decea55c0dc89c1bcd249cb2db224713
MRSevim <lionrasit@gmail.com>
2025-11-12 18:49:26 +0300

    added crosssubcookies to frontend to test

--- frontend/src/features/auth/lib/authClient.ts
+++ frontend/src/features/auth/lib/authClient.ts
@@ -3,6 +3,12 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL!,
+  advanced: {
+    crossSubDomainCookies: {
+      enabled: process.env.ENV === "production",
+      domain: process.env.BASE_DOMAIN!,
+    },
+  },
   plugins: [
     inferAdditionalFields({
       user: {
