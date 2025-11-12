58d4d1274c09c524d321e45e4d096b6ba40d7303
MRSevim <lionrasit@gmail.com>
2025-11-12 20:41:58 +0300

    added console log for debugging in prod

--- backend/src/middlewares/socketMiddleware.ts
+++ backend/src/middlewares/socketMiddleware.ts
@@ -10,6 +10,7 @@ export const useSocketMiddleware = (io: Io) => {
     try {
       // 1️⃣ Parse cookies manually from handshake headers
       const cookies = parse(socket.handshake.headers.cookie || "");
+      console.log(cookies);
       // 🔑 Use the headers from the socket handshake
       const session = await auth.api.getSession({
         headers: fromNodeHeaders(socket.handshake.headers),
