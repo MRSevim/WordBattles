efe6d7a6d870c75289e0d724ca43c55478f76584
MRSevim <lionrasit@gmail.com>
2025-11-12 17:10:18 +0300

    added class initialization to headers for backend fetch

--- frontend/src/utils/fetcher.tsx
+++ frontend/src/utils/fetcher.tsx
@@ -2,7 +2,7 @@
 import { headers } from "next/headers";
 export const fetchFromBackend = async (endpoint: string, options = {}) => {
   return await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api${endpoint}`, {
-    headers: await headers(),
+    headers: new Headers(await headers()),
     ...options,
   });
 };
